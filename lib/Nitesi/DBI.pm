793b988c0e28520690ddc90a5b212c9256d1191f
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-06-20 09:23:35 +0200

    Bump up version number.

--- lib/Nitesi/DBI.pm
+++ lib/Nitesi/DBI.pm
@@ -10,11 +10,11 @@ Nitesi::DBI - DBI bundle for Nitesi shop machine
 
 =head1 VERSION
 
-Version 0.0082
+Version 0.0090
 
 =cut
 
-our $VERSION = '0.0082';
+our $VERSION = '0.0090';
 
 =head1 DESCRIPTION
 
