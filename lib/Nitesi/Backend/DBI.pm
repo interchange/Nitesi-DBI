ff3eb414573d578bee98bc8ca74ff924361b907d
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-08-19 10:35:40 +0200

    Check whether there is a base role when handling subclasses for assignments.

--- lib/Nitesi/Backend/DBI.pm
+++ lib/Nitesi/Backend/DBI.pm
@@ -702,7 +702,12 @@ sub _manage_assignments {
 
     # handle subclassed Nitesi roles
     if (exists $info_from->{base}) {
-        $role_from = $info_from->{base};
+        if (exists $info->{assign}->{$info_from->{base}}) {
+            $role_from = $info_from->{base};
+        }
+        else{
+            $role_from = $info_from->{inherit};
+        }
     }
 
     # determine assignment parameters
